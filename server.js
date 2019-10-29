6a6c98efa0262eca5bead1fabfb1401876b72631
Evan Vogts <evanjvogts@gmail.com>
2019-10-29 15:27:50 -0500

    added genre searching functionality

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const Movies = require('./movies-data-small.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+//validates auth token
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  console.log('validate bearer token middleware');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
+
+app.get('/movies', (req, res) => {
+  const {genre, country, avg_vote} = req.query;
+  let results = Movies;
+  if(genre) {
+    if(!['Animation', 'Drama', 'Romantic', 'Comedy', 'Spy', 'Crime', 'Thriller', 'Adventure', 'Documentary', 'Horror', 'Action', 'Western', 'History', 'Biography', 'Musical', 'Fantasy', 'War', 'Grotesque'].includes(genre)) {
+      return res
+        .status(400)
+        .send('Must choose a valid genre type');
+    }
+  }
+  if (genre) {
+    results = results
+      .filter(app => app.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
+  res
+    .json(results);
 });
 
 const PORT = 8000;
